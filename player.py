e7eac663a04cbb240b9a82dbe4d6c939e4488a7d
Jocimar Lopes <45192419+jocimarlopes@users.noreply.github.com>
2019-05-02 21:49:16 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,15 +1,19 @@
 import time
 import playsound
 
-time.sleep(1)
 print("Player MP3")
-time.sleep(1)
 print("Em desenvolvimento - Jocimar Lopes")
 time.sleep(1)
 print("50% [========        ]")
 time.sleep(1)
 print("100%[================]")
+time.sleep(1)
 
+print("-=-"*15)
+print("Música na pasta: 01.mp3 (Amor pra Recomeçar - Frejat)")
+print("-=-"*15)
+
+print("Como EXEMPLO digite: 01")
 music = input('Qual a musica: ')
 playmusic = music + ".mp3"
 
