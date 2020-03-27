276eda38b43ee635d71fde8e58b52e9ee8ba017e
jocimarlopes <lopes.jocimar@gmail.com>
2020-03-26 23:55:55 -0300

    Mudança de código

--- player.py
+++ player.py
@@ -4,11 +4,6 @@ import playsound
 print("Player MP3")
 print("Em desenvolvimento - Jocimar Lopes")
 time.sleep(1)
-print("50% [========        ]")
-time.sleep(1)
-print("100%[================]")
-time.sleep(1)
-
 print("-=-"*15)
 print("Música na pasta: 01.mp3 (Amor pra Recomeçar - Frejat)")
 print("-=-"*15)
@@ -18,3 +13,5 @@ music = input('Qual a musica: ')
 playmusic = music + ".mp3"
 
 playsound.playsound(playmusic)
+print("Fim do sistema")
+time.sleep(5)
